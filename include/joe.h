960c07ecbdca70a3eb5f9a96f00bfd80522d2ccb
Michal Vyskocil <michalvyskocil@eaton.com>
2016-12-06 09:49:18 +0100

    Problem: need to build the project
    Solution: bootstrap with zproject

---
+++ include/joe.h
@@ -0,0 +1,22 @@
+/*  =========================================================================
+    joe - Joe example server
+
+    Copyright (c) the Contributors as noted in the AUTHORS file.       
+    This file is part of CZMQ, the high-level C binding for 0MQ:       
+    http://czmq.zeromq.org.                                            
+                                                                       
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.           
+    =========================================================================
+*/
+
+#ifndef JOE_H_H_INCLUDED
+#define JOE_H_H_INCLUDED
+
+//  Include the project library file
+#include "joe_library.h"
+
+//  Add your own public definitions here, if you need them
+
+#endif
