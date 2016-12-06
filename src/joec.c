dd2a8ec04c0ebbdb817d6f4b3ef05d9ab4591934
Jana Rapava <janarapava@eaton.com>
2016-12-06 13:38:01 +0100

    Problem: Stable version from yesterday is hard to change because it uses zmsg directly.
    Solution: Make it use zproto.

--- src/joec.c
+++ src/joec.c
@@ -20,6 +20,96 @@
 
 #include "joe_classes.h"
 
+static void s_actor_client(zsock_t *pipe, void *args) {
+    char *name = strdup ((char*) args);
+    zsock_t *client = zsock_new_dealer ("tcp://192.168.1.153:5000");
+    zpoller_t *poller = zpoller_new (pipe, client, NULL);
+
+    // to signal to runtime it should spawn the thread
+    zsock_signal (pipe, 0);
+    zsys_debug ("%s:\tstarted", name);
+    char *filename_full = "/etc/passwd";
+    char *filename_target = "passwd";
+    FILE *file = fopen (filename_full, "r");
+    assert (file);
+    unsigned long file_size_so_far = 0;
+    unsigned int checksum = 0;
+    int finished = 0;
+
+    joe_proto_t *msg = joe_proto_new ();
+    joe_proto_set_id (msg, JOE_PROTO_HELLO);
+    joe_proto_set_filename (msg, filename_target);
+ 
+    joe_proto_print (msg);
+    joe_proto_send (msg, client);
+    //TODO: destroy?
+
+    while (!zsys_interrupted) {
+
+        void *which = zpoller_wait (poller, -1);
+
+        if (!which)
+            break;
+
+        if (which == pipe) {
+            zmsg_t *msg = zmsg_recv (pipe);
+            char *command = zmsg_popstr (msg);
+            zsys_info ("Got API command=%s", command);
+            if (streq (command, "$TERM")) {
+                zstr_free (&command);
+                zmsg_destroy (&msg);
+                break;
+            }
+            zstr_free (&command);
+            zmsg_destroy (&msg);
+        }
+
+        if (which == client) {
+            joe_proto_recv (msg, client);
+            //zsys_info ("Received this message:");
+            joe_proto_print (msg);
+            int result = joe_proto_id (msg);
+            if (result != JOE_PROTO_READY) {
+              zsys_info("Error in transfer, client aborting.");
+              joe_proto_destroy (&msg);
+              break;
+            }
+            if (finished) {
+              zsys_info("Happy end.");
+              joe_proto_destroy (&msg);
+              break;
+            }
+
+            joe_proto_t *msg = joe_proto_new();
+   
+            char buf[100];
+            unsigned int count = fread (buf, 1, sizeof(buf), file);
+            zsys_info ("Read %u bytes from file\n", count);
+            zchunk_t *filedata = zchunk_new ((const void *) buf, count);
+            if (count > 0) {
+              joe_proto_set_id (msg, JOE_PROTO_CHUNK);
+              joe_proto_set_filename (msg, filename_target);
+              joe_proto_set_offset (msg, scanf ("%lu", &file_size_so_far));
+              joe_proto_set_size (msg, scanf ("%u", &count));
+              joe_proto_set_checksum (msg, scanf ("%u", &checksum)); //TODO: checksum
+              joe_proto_set_data (msg, &filedata);
+              file_size_so_far += count;
+            } else {
+              joe_proto_set_id (msg, JOE_PROTO_CLOSE);
+              joe_proto_set_filename (msg, filename_target);
+              joe_proto_set_size (msg, scanf ("%lu", &file_size_so_far));
+              finished = 1;
+            }
+            joe_proto_print (msg);
+            joe_proto_send (msg, client);
+            zchunk_destroy (&filedata);
+        }
+    }
+
+    joe_proto_destroy (&msg);
+    zsock_destroy (&client);
+}
+
 int main (int argc, char *argv [])
 {
     bool verbose = false;
@@ -44,5 +134,10 @@ int main (int argc, char *argv [])
     //  Insert main code here
     if (verbose)
         zsys_info ("joec - Joe client");
+    
+    zactor_t *client = zactor_new (s_actor_client, "joec"); 
+    zclock_sleep (5000);
+    zactor_destroy(&client);
+    
     return 0;
 }
